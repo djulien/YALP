3a318616a6527efebfc0d81d7ffd547bcf22d47a
djulien <djulien17@thejuliens.net>
2015-09-25 23:27:27 -0700

    update
    update

--- scripts/symlinks.js
+++ scripts/symlinks.js
@@ -1,28 +1,35 @@
 //set up symlinks
 
+var DEBUG = false;
 var colors = require('colors');
-var shell = require('shelljs/global');
+var shell = require('shelljs/global'); //https://www.npmjs.com/package/shelljs
+
+//http://blog.keithcirkel.co.uk/how-to-use-npm-as-a-build-tool/
+
+exec('node --version', exec_out);
+exec('npm --version', exec_out);
+
 
 //sym link my-plugins so they can be "require"d:
 console.log("sym link my-plugins ...".green);
 cd('my-plugins');
-exec('npm link', exec_out);
+if (!DEBUG) exec('npm link', exec_out);
 cd('../node_modules');
-exec('npm link my-plugins', exec_out);
+if (!DEBUG) exec('npm link my-plugins', exec_out);
 cd('..');
 
 //sym link my-projects so they can be "require"d:
 console.log("sym link my-plugins ...".green);
 cd('my-projects');
-exec('npm link', exec_out);
+if (!DEBUG) exec('npm link', exec_out);
 cd('../node_modules');
-exec('npm link my-projects', exec_out);
+if (!DEBUG) exec('npm link my-projects', exec_out);
 cd('..');
 
 console.log("done!".green);
 
 
-exec_out(code, output)
+function exec_out(code, output)
 {
     console.log('code: ', code, 'output:', output);
 }
