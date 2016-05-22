52b0af7994f9cd6fb5a50259b822d7daf51ecaf5
djulien <djulien17@thejuliens.net>
2016-05-21 20:12:38 -0700

    create pattern for multi-core stream source + sink

--- tests/multi-core-test.js
+++ tests/multi-core-test.js
@@ -8,11 +8,19 @@
 
 'use strict';
 require('colors');
+//require('my-plugins/my-extensions/json-revival');
 const fs = require('fs');
-const Readable = require('stream').Readable;
-const Writable = require('stream').Writable;
+//const JsonStreamer = require('stream-json/Streamer');
+//const through2 = require('through2');
+//const readline = require('readline');
+//const Readable = require('stream').Readable;
+//const Writable = require('stream').Writable;
+const stream = require('stream');
 const childproc = require('child_process');
 const OS = require('os');
+const datefmt = require('dateformat');
+const buffer = require('buffer');
+buffer.INSPECT_MAX_BYTES = 120;
 
 
 //stream api https://nodejs.org/docs/latest/api/stream.html
@@ -22,50 +30,74 @@ const OS = require('os');
 
 function main()
 {
-	console.log("i'm on cpu %d/%d".blue, getPSR(process.pid), OS.cpus().length);
+//	console.log("i'm on cpu %d/%d".blue, getPSR(process.pid), OS.cpus().length);
 //	step(gen_test());
 //	return;
 
-//	var info = gen.next().value;
-	var src = step(rdstm());
+//	create();
+	playback();
+}
+
+
+function create()
+{
+	var src = rdstm();
 //	src.setEncoding('utf8'); //return data as strings
 //	src.setEncoding('hex'); //return data as hex strings
 //	src.resume(); //start flow
-	if (false)
+//	var obj2txt = through2.obj(function(chunk, encoding, next) { this.push(JSON.stringify(chunk, null, 4) + '\n'); next(); })
+//	var obj2txt = new stream.Transform({transform: function(chunk, encoding, next) { this.push(JSON.stringify(chunk) + '\n'); next(); }});
+
+//	if (false)
 	{
+		src = src.pipe(Object2Text());
 		var sink = fs.createWriteStream('stream.txt');
 		src.pipe(sink);
-		src.pipe(watch(process.stdout, "stdout"), {end: true}); //start flow, end writer when reader ends
+		src.pipe(watch(process.stdout, "stdout")); //, {end: true}); //start flow, end writer when reader ends
 	}
-//	var n = 0;
-//	if (false)
+	if (false)
 	setTimeout(function()
 	{
 		for (var i = 0; i < 5; ++i) console.log(i, src.read());
 	}, 5000);
-//	src.on('readable', function() { if (++n < 6) console.dir(src.read()); }); //data is available
+}
+
+
+function playback()
+{
+//	var n = 0; src.on('readable', function() { if (++n < 6) console.dir(src.read()); }); //data is available
 //	process.stdin.pipe(wr());
-	return;
-	var sink = step(wrstm());
+	var src = fs.createReadStream('stream.txt');
+	src = src.pipe(Text2Object());
+//	const rl = readline.createInterface({input: src});
+//	rl.on('line', (line) => { console.log('Line from file:', line); });
+
+	var sink = wrstm();
 //	sink.write("hello");
 //	sink.end("bye");
+	src.pipe(Object2Text())
+		.pipe(watch(process.stdout, "stdout")); //, {end: true}); //start flow, end writer when reader ends
 	src.pipe(sink);
-//	src.pipe(process.stdout, {end: true}); //start flow, end
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////
 ////
-/// readable
+/// readable stream (render)
 //
 
 //readable stream:
-//generator + yield allows async code to be written using sync style
-function* rdstm()
+//send rendered frame data
+function rdstm()
 {
-	const epoch = Date.now(); //elapsed();
-	console.log("rd stm created", '@' + elapsed(epoch));
-	var rs = watch(new Readable({objectMode: true, highWaterMark: 2, read: function(size_ignored) { this.myit.step(); } }), "readable"); //read ahead max 2 frames
+	return step(inner());
+//generator + yield allows async code to be written using sync style
+function* inner()
+{
+//	const epoch = Date.now(); //elapsed();
+	console.log("rd stm created", stamp(null)); //epoch));
+	var rs = watch(new stream.Readable({objectMode: true, highWaterMark: 2, read: function(size_ignored) { step(this.myit); } }), "readable"); //read ahead max 2 frames
+	rs.desc = "read stream"; //for easier debug
 //	var old_push = rs.push; rs.push = function(str) { return old_push.call(rs, (str !== null)? str + '\n': str); };
 //    var old_write = port.inbuf.write;
 //    port.inbuf.write = function(args) { logger("inbuf write"); return old_write.apply(port.inbuf, arguments); }
@@ -83,10 +115,10 @@ function* rdstm()
 		return {rstrm: this, pushret: this.push(str)};
 	};
 */
-//	rs._read = function(size_ignored) { this.myit.step(); } //wake up stream when consumer wants more data
+//	rs._read = function(size_ignored) { step(this.myit); } //wake up stream when consumer wants more data
 
-	const seqlen = 1000-2, interval = 50;
-	for (var frnum = 0, delay = 0; delay < seqlen + interval; ++frnum, delay += interval)
+	const seqlen = 5*1000-2, interval = 20*50, numintv = Math.ceil(seqlen / interval);
+	for (var frnum = 0, delay = 0; frnum <= numintv /*delay < seqlen + interval*/; ++frnum, delay += interval)
 	{
 		var wait4req = frame(delay, 'frame ' + frnum);
 		if (wait4req) yield wait4req; //wait until sink wants more data
@@ -94,57 +126,174 @@ function* rdstm()
 	}
 //	yield rs; //wait until reader wants data
 //	rs.pushline('-eof-');
-	console.log("rd stm eof", '@' + elapsed(epoch));
+	console.log("rd stm eof", stamp());
 	rs.push(null); //eof
+//	rs.close();
 //	return rs;
 
 	function frame(delay, data) //package and send next frame
 	{
-		var buf = data = {delay: delay, data: data};
+debugger;
+		var buf = {delay: delay, data: data};
 //		if (rs.isTTY && (str !== null)) str += '\n';
-		if (typeof buf !== 'String') buf = JSON.stringify(data) + '\n'; //make compatible with text streams; must be string or buffer, not object
+//		if ((typeof data == 'string') || Buffer.isBuffer(data)) return data; //okay to send as-is
+//		if (typeof buf !== 'String')) buf = JSON.stringify(data) + '\n'; //make compatible with text streams; must be string or buffer, not object
+		if (!delay) rs.push({delay: -1, seqlen: seqlen, interval: interval, numintv: numintv, comment: 'created ' + datefmt(Date.now())});
 		var want_more = rs.push(buf); //enque data immediately so it's available on demand, *then* wait until reader wants more
-		console.log("rd stm more? %s, pushed", want_more, data, '@' + elapsed(epoch));
+		console.log("rd stm more? %s, pushed", want_more, buf, stamp());
 		return !want_more? function(myit) { /*myit.step()*/ rs.myit = myit; return rs; }: null;
 	}
+}
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////
 ////
-/// writable
+/// writable stream (hardware control)
 //
 
 //writable stream:
-//generator + yield allows async code to be written using sync style
-function* wrstm()
+//sends data to hardware using precise timing
+function wrstm()
 {
-	const epoch = Date.now(); //elapsed();
-	console.log("wr stm created", '@' + elapsed(epoch));
-	var ws = watch(new Writable({objectMode: true, highWaterMark: 2, xdecodeStrings: true }), "writable"); //write ahead max 2 frames
-/*
+//	const epoch = Date.now(); //elapsed();
+	console.log("wr stm created", stamp(null));
+//	var ws = watch(new stream.Writable(
+	var ws = watch(new stream.Transform( //kludge: Writable uses Socket which rejects objects, so use inbound side of Transform instead
+	{
+		objectMode: true,
+//		readableObjectMode: true,
+		highWaterMark: 2, //read ahead max 2 frames
+		decodeStrings: true,
+//		write: function(chunk, enc, next_cb)
+		transform: function(chunk, enc, done_cb)
+		{
+debugger;
+//			if (typeof chunk === 'String') chunk = JSON.parse(chunk, enc);
+//			chunk = JSON.parse(chunk, enc); //only needed if options.decodeStrings == false
+//			if ((typeof chunk !== 'String') && !Buffer.isBuffer(chunk)) chunk = JSON.stringify(chunk) + '\n'; //make compatible with text streams; must be string or buffer
+//			step(this.myit, chunk);
+//			this.push(chunk);
+			var delay = 0;
+			if (typeof chunk.delay != 'undefined')
+			{
+//??				if (chunk.delay === 0) epoch = Date.now(); //reset to account for startup latency
+				delay = chunk.delay - elapsed();
+			}
+			console.log("delay %d msec for:", delay, chunk, stamp());
+			if (delay > 0) setTimeout(function() { console.log("wr", chunk.data, stamp()); done_cb(); }, delay); //done_cb(); //tell sender to write more data
+			else { console.log("wr immed", chunk.data || chunk.comment, stamp()); done_cb(); }
+		},
+	}), "writable");
+	ws.desc = "write stream"; //for easier debug
+	return ws;
+
+	return step(inner());
+//generator + yield allows async code to be written using sync style
+function* inner()
+{
+//	const epoch = Date.now(); //elapsed();
+	console.log("wr stm created", stamp(null));
+	var ws = watch(new stream.Writable({objectMode: true, highWaterMark: 2, xdecodeStrings: true }), "writable"); //write ahead max 2 frames
+	ws.desc = "write stream"; //for easier debug
 	ws._write = function(chunk, enc, next)
 	{
-//		if (typeof chunk === 'String') decode;
-		debugger;
+debugger;
+//		if (typeof chunk === 'String') chunk = JSON.parse(chunk, enc);
+//		chunk = JSON.parse(chunk, enc); //only needed if options.decodeStrings == false
 		console.log("write:", chunk);
-		next(); //tell consumer to write more data
+//		step(this.myit, chunk);
+		next(); //tell sender to write more data
 	};
-	return ws;
-*/
 
+//	yield wait4frame();
 	for (;;)
 	{
 		var frame = yield wait4frame();
-		console.log("wr stm", frame, '@' + elapsed(epoch));
+		console.log("wr stm", frame, stamp());
 		if (!frame) break; //eof
+//		yield wait(frame.delay);
 	}
-	console.log("wr stm eof", '@' + elapsed(epoch));
+	console.log("wr stm eof", stamp());
 
 	function wait4frame()
 	{
-		return function(myit) { /*myit.step()*/ rs.myit = myit; return rs; };
+		return function(myit) { ws.myit = myit; return ws; };
 	}
+}
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////
+////
+/// transforms
+//
+
+function Object2Text()
+{
+	var retval = inner();
+	retval.desc = "object to text"; //for easier debug
+	return retval;
+function inner()
+{
+	return new stream.Transform(
+	{
+		readableObjectMode: true,
+		writableObjectMode: true, //NOTE: required; allows objects to be received without error, then stringified
+		transform: function(chunk, enc, done_cb)
+		{
+//console.log("obj2txt:", chunk, enc);
+			if ((typeof chunk !== 'String') && !Buffer.isBuffer(chunk)) chunk = JSON.stringify(chunk) + '\n'; //make compatible with text streams; must be string or buffer
+			this.push(chunk);
+			done_cb();
+		},
+	});
+}
+}
+
+
+//parse JSON text lines back into objects:
+//see example at https://nodejs.org/api/stream.html
+function Text2Object()
+{
+	var retval = inner();
+	retval.desc = "text to object"; //for easier debug
+	return retval;
+function inner()
+{
+	return new stream.Transform(
+	{
+		readableObjectMode: true, //NOTE: required; allows objects to be written
+//		writableObjectMode: true, //NOTE: required; allows delimited strings to be parsed back into objects
+		transform: function(chunk, enc, done_cb)
+		{
+//console.log("txt2obj:", chunk, enc);
+debugger;
+//			var data = (enc === 'buffer')? 
+//				JSON.parse(chunk, (key, value) => { return (value && (value.type === 'Buffer'))? Buffer.from(value.data): value; }):
+//				chunk.toString(enc);
+			var data = (enc !== 'buffer')? chunk.toString(enc): chunk.toString(); //NOTE: can't use JSON.parse yet because text line might be incomplete
+			if (this.linebuf) data = this.linebuf + data;
+			var lines = data.split(/\r?\n/);
+			this.linebuf = lines.pop(); //splice(lines.length-1,1)[0]; //save last partial line for next time
+//			lines.forEach(this.push.bind(this));
+			lines.forEach(function(line) { this.push(parse(line)); }.bind(this));
+			done_cb();
+		},
+		flush: function(done_cb)
+		{
+			if (this.linebuf) this.push(parse(this.linebuf));
+			this.linebuf = null;
+			done_cb();
+		},
+	});
+
+	function parse(str)
+	{
+debugger;
+		return JSON.parse(str, (key, value) => { return (value && (value.type === 'Buffer'))? Buffer.from(value.data): value; });
+	}
+}
 }
 
 
@@ -154,12 +303,12 @@ function* wrstm()
 //
 
 //watch for events on a stream:
-function watch(str, desc)
+function watch(stm, desc)
 {
-	return str
+	return stm
 //	var x; x
 	        .on('open', function() { console.log("opened %s".green, desc); })
-	        .on('readable', function(data) { console.log("readable %s".blue, desc); data = null; })
+//CAUTION: leaves process stuck at end	        .on('readable', function(data) { console.log("readable %s".blue, desc); data = null; })
 //CAUTION: triggers classic mode	        .on('data', function (data) { console.log("data %s".cyan, desc); data = null; })
 	        .on('drain', function() { console.log("drained %s".green, desc); }) //writable only
 	        .on('pipe', function(src) { console.log("piped %s".cyan, desc); })
@@ -181,13 +330,16 @@ function watch(str, desc)
 
 //step a generator function:
 //returns intermediate or final value
-function step(gen)
+function step(gen, args)
 {
-	debugger;
+//	console.log("step ...", arguments.length, gen);
+debugger;
 //	console.log(typeof gen);
-	var retval = gen.next(); //step generator function to next yield
-	gen.step = function() { return step(gen); } //allow next step to call this function using oo syntax
+	args = Array.from(arguments); args.shift();
+	var retval = gen.next.apply(gen, args); //send remaining args to next yield within generator function
+//	gen.step = function(args) { args = Array.from(arguments); args.unshift(gen); return step.apply(args); } //allow next step to call this function using oo syntax
 //	return retval.done? retval.value: retval.value(gen);
+//	console.log("... step");
 	return (typeof retval.value == 'function')? retval.value(gen): retval.value;
 }
 
@@ -209,6 +361,10 @@ function step(gen)
 
 //get processor of a process:
 //from http://stackoverflow.com/questions/30496989/determine-which-core-is-running-node-js-process-at-runtime
+function cpuid()
+{
+//	console.log("i'm on cpu %d/%d".blue, getPSR(process.pid), OS.cpus().length);
+	return getPSR(process.pid) + '/' + OS.cpus().length;
 function getPSR(pid) //, callback)
 {
     var exec = childproc.execSync;
@@ -216,13 +372,21 @@ function getPSR(pid) //, callback)
     var result = exec(command);
     return result.toString("utf-8").trim();
 }
+}
 
 
 //elapsed time:
 function elapsed(epoch)
 {
-	if (!elapsed.start) elapsed.start = epoch || Date.now(); // + (earlier || 0);
+	if (!elapsed.start || (epoch === null)) elapsed.start = epoch || Date.now(); // + (earlier || 0);
+//	console.log("elapsed: (%s || %s) - %s = %s", epoch, Date.now(), elapsed.start, (epoch || Date.now()) - elapsed.start); //msec
 	return (epoch || Date.now()) - elapsed.start; //msec
+}
+
+
+function stamp(epoch)
+{
+	return '@' + elapsed(epoch) + '&' + cpuid();
 }
 
 
@@ -246,14 +410,14 @@ function cpu_loop()
 //wraps async delays using sync syntax
 function* gen_test()
 {
-	console.log("test start", '@' + elapsed());
+	console.log("test start", stamp());
 	yield wait(1000, true);
 	for (var i = 0; i < 5; ++i)
 	{
-		console.log("test " + i, '@' + elapsed());
+		console.log("test " + i, stamp());
 		yield wait(2000, true);
 	}
-	console.log("test end", '@' + elapsed());
+	console.log("test end", stamp());
 	return -1;
 
 	function wait(delay, adjust) //return a dispatcher function
@@ -265,13 +429,21 @@ function* gen_test()
 		delay -= adjust; //compensate for inaccurate timer
 		return function(myit)
 		{
-			if (delay > 0) setTimeout(function() { myit.step(); }, delay);
-			else myit.step(); //no delay or overdue
+			if (delay > 0) setTimeout(function() { step(myit); }, delay);
+			else step(myit); //no delay or overdue
 		}
 	}
 }
 
 
 main(); //must be at eof to errors due to avoid hoisting
+
+//http://stackoverflow.com/questions/17960452/how-can-i-get-a-list-of-callbacks-in-the-node-work-queue-or-why-wont-node-ex
+if (false)
+setTimeout(function()
+{
+    console.log(process._getActiveHandles());
+    console.log(process._getActiveRequests());
+}, 10000);
 
 //eof
